7a9149e11dcd4ed03995ccff3219e9121750c6b8
Rio Liu <rio@r26.me>
2026-01-30 10:32:34 -0500

    roll back from awesome-git

--- rc-laptop.lua
+++ rc-laptop.lua
@@ -33,13 +33,19 @@ local player_widget = require("player_widget")
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
-naughty.connect_signal("request::display_error", function(message, startup)
-    naughty.notification {
-        urgency = "critical",
-        title   = "Oops, an error happened"..(startup and " during startup!" or "!"),
-        message = message
-    }
-end)
+do
+    local in_error = false
+    awesome.connect_signal("debug::error", function (err)
+        -- Make sure we don't go into an endless error loop
+        if in_error then return end
+        in_error = true
+
+        naughty.notify({ preset = naughty.config.presets.critical,
+                         title = "Oops, an error happened!",
+                         text = tostring(err) })
+        in_error = false
+    end)
+end
 -- }}}
 
 -- Variables
